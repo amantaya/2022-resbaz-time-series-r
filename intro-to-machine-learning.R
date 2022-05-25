8e2fd646dccf49a8b31bdc4de219cc2fd3e4105e
Andrew Antaya <aantaya@email.arizona.edu>
2022-05-25 13:44:12 -0700

    initial commit

---
+++ intro-to-machine-learning.R
@@ -0,0 +1,16 @@
+# install package “VIM”
+# install.packages("VIM")
+
+# To use the package in an R session, we need to load it in an R session via
+library(VIM)
+
+# Load dataset “sleep”, which comes within the package “VIM”
+data(sleep, package = "VIM")
+
+# call function head() to get a feeling about data, or call sleep to see all values
+head(sleep)
+
+# download package “mice” and load it into R
+# install.packages("mice")
+library(mice)
+
